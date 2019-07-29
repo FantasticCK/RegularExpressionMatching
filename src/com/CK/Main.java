77d26072d92e24b41b9cacd101d9468bf2277dd1
CK <kainingc@usc.edu>
2019-07-29 12:50:03 -0700

    Modify Bottom-up

--- src/com/CK/Main.java
+++ src/com/CK/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         String s = "";
         String p = "c*c*";
-        Solution solution = new Solution();
+        Solution2 solution = new Solution2();
         System.out.println(solution.isMatch(s, p));
     }
 }
@@ -58,5 +58,73 @@ class Solution {
             }
         }
         return pSB.toString();
+    }
+}
+
+//DP Bottom-up
+class Solution2 {
+    public boolean isMatch(String s, String p) {
+
+        if (s == null || p == null) {
+            return false;
+        }
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '*' && dp[0][i - 1]) {
+                dp[0][i + 1] = true;
+            }
+        }
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (p.charAt(j) == '.') {
+                    dp[i + 1][j + 1] = dp[i][j];
+                }
+                if (p.charAt(j) == s.charAt(i)) {
+                    dp[i + 1][j + 1] = dp[i][j];
+                }
+                if (p.charAt(j) == '*') {
+                    if (p.charAt(j - 1) != s.charAt(i) && p.charAt(j - 1) != '.') {
+                        dp[i + 1][j + 1] = dp[i + 1][j - 1];
+                    } else {
+                        dp[i + 1][j + 1] = (dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1]);
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
+}
+
+//DP Top-bottom
+class Solution3 {
+    Boolean[][] memo;
+
+    public boolean isMatch(String text, String pattern) {
+        memo = new Boolean[text.length() + 1][pattern.length() + 1];
+        return dp(0, 0, text, pattern);
+    }
+
+    public boolean dp(int i, int j, String text, String pattern) {
+        if (memo[i][j] != null) {
+            return memo[i][j];
+        }
+        boolean ans;
+        if (j == pattern.length()) {
+            ans = i == text.length();
+        } else {
+            boolean first_match = (i < text.length() &&
+                    (pattern.charAt(j) == text.charAt(i) ||
+                            pattern.charAt(j) == '.'));
+
+            if (j + 1 < pattern.length() && pattern.charAt(j + 1) == '*') {
+                ans = (dp(i, j + 2, text, pattern) ||
+                        first_match && dp(i + 1, j, text, pattern));
+            } else {
+                ans = first_match && dp(i + 1, j + 1, text, pattern);
+            }
+        }
+        memo[i][j] = ans;
+        return ans;
     }
 }
